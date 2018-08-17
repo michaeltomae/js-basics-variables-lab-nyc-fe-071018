88565edaffeabc0769d8086c71253b08800b4184
Michael Tomae <michael.tomae@gmail.com>
2018-08-17 14:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 // Code your solution in this file!
-
-describe('index.js', function () {
-  describe('companyName', function () {
-    it('is set as Scuber', function () {
-      expect(companyName).to.equal('Scuber');
-    });
-
-    it('raises error if the companyName is changed', function () {
-      expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
-    });
-  });
+const companyName = 'Scuber';
