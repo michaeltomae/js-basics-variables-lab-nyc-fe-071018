a488e63e8e810d89a46991fb73181518be67af35
Michael Tomae <michael.tomae@gmail.com>
2018-08-17 14:02:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Code your solution in this file!
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith';
+
+describe('index.js', function () {
+  describe('companyName', function () {
+    it('is set as Scuber', function () {
+      expect(companyName).to.equal('Scuber');
+    });
+
+    it('raises error if the companyName is changed', function () {
+      expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+    });
+  });
